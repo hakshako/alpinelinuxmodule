3c542bdfebe9e3625702dc23efd4183417679640
HaksHako <96785181+hakshako@users.noreply.github.com>
2026-03-02 01:48:44 +0300

    Refactor service.sh for improved structure and clarity

--- service.sh
+++ service.sh
@@ -13,33 +13,36 @@ fi
 sleep 20
 
 nsenter --mount=/proc/1/ns/mnt -- sh -c "
-mkdir -p "$ROOTFS/dev" "$ROOTFS/dev/pts" "$ROOTFS/proc" "$ROOTFS/sys" "$ROOTFS/dev/shm" "$ROOTFS/system" "$ROOTFS/vendor" "$ROOTFS/home"
-mount -o bind /dev "$ROOTFS/dev"
-mount -o bind /proc "$ROOTFS/proc"
-mount -o bind /sys "$ROOTFS/sys"
-mount -t devpts devpts "$ROOTFS/dev/pts"
-mount -t tmpfs -o size=128M tmpfs "$ROOTFS/dev/shm"
-mount -o bind /vendor "$ROOTFS/vendor"
-mount -o bind /system "$ROOTFS/system"
-mount -o bind /sdcard/home "$ROOTFS/home"
+
+mkdir -p $ROOTFS/dev $ROOTFS/dev/pts $ROOTFS/proc \
+         $ROOTFS/sys $ROOTFS/dev/shm \
+         $ROOTFS/system $ROOTFS/vendor \
+         $ROOTFS/home $ROOTFS/apex
+
+mount -o bind /dev $ROOTFS/dev
+mount -o bind /proc $ROOTFS/proc
+mount -o bind /sys $ROOTFS/sys
+mount -o bind /vendor $ROOTFS/vendor
+mount -o bind /system $ROOTFS/system
+mount -o bind /apex $ROOTFS/apex
+mount -t devpts devpts $ROOTFS/dev/pts
+mount -t tmpfs -o size=128M tmpfs $ROOTFS/dev/shm
+mount -o bind /sdcard/home $ROOTFS/home
+
+echo 'nameserver 1.1.1.1' > $ROOTFS/etc/resolv.conf
+echo 'nameserver 1.0.0.1' >> $ROOTFS/etc/resolv.conf
+
+if [ ! -f $ROOTFS/usr/sbin/sshd ]; then
+  chroot $ROOTFS /bin/sh -c '
+    export PATH=/usr/sbin:/usr/bin:/sbin:/bin
+    apk update
+    apk add openssh nano
+    ssh-keygen -A
+  '
+fi
+
+chroot $ROOTFS /usr/sbin/sshd -p 8022
+
 "
-echo "nameserver 1.1.1.1" > "$ROOTFS/etc/resolv.conf"
-echo "nameserver 1.0.0.1" >> "$ROOTFS/etc/resolv.conf"
-
-if [ ! -f "$ROOTFS/usr/sbin/sshd" ]; then
-    $BUSYBOX chroot "$ROOTFS" /bin/sh -c "
-        export PATH=/usr/sbin:/usr/bin:/sbin:/bin
-        apk update
-        apk add openssh
-        apk add nano
-        ssh-keygen -A
-    "
-fi
-
-if [ -f "$ROOTFS/usr/sbin/sshd" ]; then
-    if ! $BUSYBOX chroot "$ROOTFS" pgrep sshd >/dev/null 2>&1; then
-        $BUSYBOX chroot "$ROOTFS" /usr/sbin/sshd -p 8022
-    fi
-fi
 
 exit 0
